e860a79da7ff35ad95440f3d61ffde5a029cd64c
AlexM <alexm.box@gmail.com>
2017-12-20 22:45:16 +0200

    Add workers timeout

--- startSGMT.sh
+++ startSGMT.sh
@@ -1,3 +1,3 @@
 cd /home/ec2-user/steamgifts-management-tool
-gunicorn --bind 0.0.0.0:8080 --workers 8 wsgi:app > /dev/null 2>&1 & echo $! > $1
+gunicorn --bind 0.0.0.0:8080 --timeout 3600 --workers 8 wsgi:app > /dev/null 2>&1 & echo $! > $1
 cat $1
