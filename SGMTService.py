8966e1ea50246ae9dd39d3d86e9c04e12f7ddf0a
AlexM <alexm.box@gmail.com>
2017-12-24 18:36:45 +0200

    Fixing time printouts

--- SGMTService.py
+++ SGMTService.py
@@ -5,6 +5,7 @@
 import json
 import time
 
+import datetime
 from flask import Flask
 from flask import request
 
@@ -242,7 +243,7 @@ def add_new_group():
     cookies = request.args.get('cookies')
     SGMTBusinessLogic.add_new_group(group_webpage, cookies)
     LogUtils.log_info('AddNewGroup ' + group_webpage + ' took ' + str(time.time() - start_time) +  ' seconds')
-    return json.dumps({'success': True, 'timestamp': time.strftime('%Y-%m-%d %H:%M:%S', time.time())}), 200, {'ContentType': 'application/json'}
+    return json.dumps({'success': True, 'timestamp':datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}), 200, {'ContentType': 'application/json'}
 
 
 @app.route('/SGMT-Admin/UpdateGroupData', methods=['GET'])
@@ -251,7 +252,7 @@ def update_group_data():
     group_webpage = request.args.get('group_webpage')
     SGMTBusinessLogic.update_existing_group(group_webpage)
     LogUtils.log_info('UpdateGroupData ' + group_webpage + ' took ' + str(time.time() - start_time) +  ' seconds')
-    return json.dumps({'success': True, 'timestamp': time.strftime('%Y-%m-%d %H:%M:%S', time.time())}), 200, {'ContentType': 'application/json'}
+    return json.dumps({'success': True, 'timestamp': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}), 200, {'ContentType': 'application/json'}
 
 
 @app.route('/SGMT-Admin/UpdateAllGroups', methods=['GET'])
@@ -261,7 +262,7 @@ def update_all_groups():
     SGMTBusinessLogic.update_all_db_games_data()
     SGMTBusinessLogic.update_all_db_groups()
     LogUtils.log_info('UpdateAllGroups took ' + str(time.time() - start_time) +  ' seconds')
-    return json.dumps({'success': True, 'timestamp': time.strftime('%Y-%m-%d %H:%M:%S', time.time())}), 200, {'ContentType': 'application/json'}
+    return json.dumps({'success': True, 'timestamp': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}), 200, {'ContentType': 'application/json'}
 
 
 @app.route('/SGMT/warranty', methods=['GET'])
