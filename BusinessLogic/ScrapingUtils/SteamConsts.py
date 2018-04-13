bd07b25169a7a5cd7242fe0c3d63c0cc0376dbc6
AlexM <alexm.box@gmail.com>
2018-04-13 06:08:13 +0300

    Link changed

--- BusinessLogic/ScrapingUtils/SteamConsts.py
+++ BusinessLogic/ScrapingUtils/SteamConsts.py
@@ -3,7 +3,7 @@
 
 
 STEAM_FILTER_LINK = 'https://steamcommunity.com/linkfilter/?url='
-STEAM_PROFILE_LINK = 'http://steamcommunity.com/profiles/'
+STEAM_PROFILE_LINK = 'https://steamcommunity.com/profiles/'
 STEAM_USER_ID_LINK = 'http://steamcommunity.com/id/'
 STEAM_GAME_LINK = 'http://store.steampowered.com/app/'
 STEAM_PACKAGE_LINK = 'http://store.steampowered.com/sub/'
