231e0b9e44dbd9f622992aa8f734e9757dbb1e78
AlexM <alexm.box@gmail.com>
2018-04-16 09:33:22 +0300

    Updating all links to https

--- BusinessLogic/ScrapingUtils/SteamConsts.py
+++ BusinessLogic/ScrapingUtils/SteamConsts.py
@@ -4,7 +4,7 @@
 
 STEAM_FILTER_LINK = 'https://steamcommunity.com/linkfilter/?url='
 STEAM_PROFILE_LINK = 'https://steamcommunity.com/profiles/'
-STEAM_USER_ID_LINK = 'http://steamcommunity.com/id/'
-STEAM_GAME_LINK = 'http://store.steampowered.com/app/'
-STEAM_PACKAGE_LINK = 'http://store.steampowered.com/sub/'
+STEAM_USER_ID_LINK = 'https://steamcommunity.com/id/'
+STEAM_GAME_LINK = 'https://store.steampowered.com/app/'
+STEAM_PACKAGE_LINK = 'https://store.steampowered.com/sub/'
 STEAM_SEARCH_QUERY = '/?ctp='
