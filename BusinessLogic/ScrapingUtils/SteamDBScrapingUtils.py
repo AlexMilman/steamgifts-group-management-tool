636dc38c6bd46c0233a5bcc0d6e90d4634fd31e5
AlexM <alexm.box@gmail.com>
2019-03-03 11:08:37 +0200

    Fixing game processing

--- BusinessLogic/ScrapingUtils/SteamDBScrapingUtils.py
+++ BusinessLogic/ScrapingUtils/SteamDBScrapingUtils.py
@@ -12,8 +12,12 @@ def get_game_additional_data(game_name, game_link):
     num_of_reviews = 0
     steam_app_id = game_link.split(SteamConsts.STEAM_GAME_LINK)[1]
     html_content = WebUtils.get_html_page(SteamDBConsts.STEAM_DB_APP_LINK + steam_app_id, https=True)
-    steam_game_tooltip = WebUtils.get_item_by_xpath(html_content, u'.//span[@class="header-thing header-thing-good tooltipped tooltipped-n"]/@aria-label')
-    if steam_game_tooltip != 'Need more user reviews to generate a score' and steam_game_tooltip != 'No user reviews':
-        steam_score = int(StringUtils.normalize_float(steam_game_tooltip.split('%')[0]))
-        num_of_reviews = StringUtils.normalize_int(steam_game_tooltip.split('of the')[1].split('user reviews')[0])
+    steam_score_positive = WebUtils.get_item_by_xpath(html_content, u'.//span[@class="header-thing-good"]/text()')
+    steam_score_negative = WebUtils.get_item_by_xpath(html_content, u'.//span[@class="header-thing-poor"]/text()')
+
+    if steam_score_positive and steam_score_negative:
+        positive_score = int(steam_score_positive)
+        negative_score = int(steam_score_negative)
+        num_of_reviews = positive_score + negative_score
+        steam_score = int(float(positive_score) / num_of_reviews * 100)
     return steam_score, num_of_reviews
