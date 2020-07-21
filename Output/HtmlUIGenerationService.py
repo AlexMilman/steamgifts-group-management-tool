661fc8d696699dfebb7a82b1225ef8434de4f680
AlexM <alexm.box@gmail.com>
2020-07-21 08:29:24 +0300

    Changing inactive user link to point to full user data

--- Output/HtmlUIGenerationService.py
+++ Output/HtmlUIGenerationService.py
@@ -158,7 +158,7 @@ def get_year_month():
     response += '<option value="2017">2017</option>'
     response += '<option value="2018">2018</option>'
     response += '<option value="2019">2019</option>'
-    response += '<option value="2020">2019</option>'
+    response += '<option value="2020">2020</option>'
     response += '</select>&nbsp; &nbsp;'
 
     response += 'Month:&nbsp; &nbsp;<select name="month">'
