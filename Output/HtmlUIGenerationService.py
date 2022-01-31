6fa7e8e1e66a9ecaad173b441e03faddb6fedc55
AlexMilman <alexm.box@gmail.com>
2022-01-31 16:02:35 +0200

    Adding support for year 2022

--- Output/HtmlUIGenerationService.py
+++ Output/HtmlUIGenerationService.py
@@ -160,6 +160,7 @@ def get_year_month():
     response += '<option value="2019">2019</option>'
     response += '<option value="2020">2020</option>'
     response += '<option value="2021">2021</option>'
+    response += '<option value="2022">2022</option>'
     response += '</select>&nbsp; &nbsp;'
 
     response += 'Month:&nbsp; &nbsp;<select name="month">'
