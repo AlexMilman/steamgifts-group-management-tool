3ddb7235adc9ef09353fd7a077c4f0b25c5ae6d1
AlexMilman <alexm.box@gmail.com>
2022-07-28 18:23:44 +0300

    Fixing minor bug

--- Database/MySqlConnector.py
+++ Database/MySqlConnector.py
@@ -440,7 +440,7 @@ def overwrite_bundled_games(bundled_games):
         bundled_games_data.append((bundled_game.app_id, bundled_game.game_name, bundled_game.was_bundled, bundled_game.was_free))
 
     cursor.execute("TRUNCATE TABLE BundledGames")
-    cursor.executemany("INSERT INTO BundledGames (AppId,GameName,WasBundled,WasFree) VALUES (%s, %s, %s, %s, %s)", bundled_games_data)
+    cursor.executemany("INSERT INTO BundledGames (AppId,GameName,WasBundled,WasFree) VALUES (%s, %s, %s, %s)", bundled_games_data)
     connection.commit()  # you need to call commit() method to save your changes to the database
 
     cursor.close()
