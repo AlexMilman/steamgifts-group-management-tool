78f4fe62e5a0f1bd8ea676d3524a7c398f141081
AlexMilman <alexm.box@gmail.com>
2023-01-20 10:25:16 +0200

    Fixing 2023 year value

--- Output/HtmlUIGenerationService.py
+++ Output/HtmlUIGenerationService.py
@@ -163,7 +163,7 @@ def get_year_month():
     response += '<option value="2020">2020</option>'
     response += '<option value="2021">2021</option>'
     response += '<option value="2022">2022</option>'
-    response += '<option value="2022">2023</option>'
+    response += '<option value="2023">2023</option>'
     response += '</select>&nbsp; &nbsp;'
 
     response += 'Month:&nbsp; &nbsp;<select name="month">'
