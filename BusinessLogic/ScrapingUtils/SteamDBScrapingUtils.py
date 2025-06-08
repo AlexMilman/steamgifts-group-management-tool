74329b30a3ea9572a7b798ea6cbb96d9d0c19791
AlexMilman <alexm.box@gmail.com>
2025-06-08 09:58:21 +0300

    Can no longer use SteamDB, as it's protected by CloudFlare.
    This could be circumvented by cloudscraper, but it's only available for Python 3

--- BusinessLogic/ScrapingUtils/SteamDBScrapingUtils.py
+++ BusinessLogic/ScrapingUtils/SteamDBScrapingUtils.py
@@ -6,19 +6,21 @@ from BusinessLogic.Utils import WebUtils, StringUtils, LogUtils
 # Copyright (C) 2017  Alex Milman
 
 
+# Cannot use this anymore - it's now protected by CloudFlare and requires a human
 def get_game_additional_data(game_name, game_link):
-    LogUtils.log_info('Processing game in SteamDB: ' + game_name)
-    steam_score = 0
-    num_of_reviews = 0
-    steam_app_id = game_link.split(SteamConsts.STEAM_GAME_LINK)[1].split('/')[0]
-    html_content = WebUtils.get_html_page(SteamDBConsts.STEAM_DB_APP_LINK + steam_app_id, https=True)
-    steam_score_positive = WebUtils.get_item_by_xpath(html_content, u'.//span[@class="header-thing-good"]/text()')
-    steam_score_negative = WebUtils.get_item_by_xpath(html_content, u'.//span[@class="header-thing-poor"]/text()')
-
-    if steam_score_positive and steam_score_negative:
-        positive_score = int(steam_score_positive)
-        negative_score = int(steam_score_negative)
-        num_of_reviews = positive_score + negative_score
-        if num_of_reviews > 0:
-            steam_score = int(float(positive_score) / num_of_reviews * 100)
-    return steam_score, num_of_reviews
+    raise Exception("Unable to use SteamDB, as it's protected by CloudFlare")
+    # LogUtils.log_info('Processing game in SteamDB: ' + game_name)
+    # steam_score = 0
+    # num_of_reviews = 0
+    # steam_app_id = game_link.split(SteamConsts.STEAM_GAME_LINK)[1].split('/')[0]
+    # html_content = WebUtils.get_html_page(SteamDBConsts.STEAM_DB_APP_LINK + steam_app_id, https=True)
+    # steam_score_positive = WebUtils.get_item_by_xpath(html_content, u'.//span[@class="header-thing-good"]/text()')
+    # steam_score_negative = WebUtils.get_item_by_xpath(html_content, u'.//span[@class="header-thing-poor"]/text()')
+    #
+    # if steam_score_positive and steam_score_negative:
+    #     positive_score = int(steam_score_positive)
+    #     negative_score = int(steam_score_negative)
+    #     num_of_reviews = positive_score + negative_score
+    #     if num_of_reviews > 0:
+    #         steam_score = int(float(positive_score) / num_of_reviews * 100)
+    # return steam_score, num_of_reviews
